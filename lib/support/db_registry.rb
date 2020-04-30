d082cc3fc3a024204d7451511c5580240c0c6982
github username <github email address>
2020-04-30 15:05:22 -0500

    Done.

--- lib/support/db_registry.rb
+++ lib/support/db_registry.rb
@@ -3,4 +3,4 @@ require_relative "./connection_adapter.rb"
 DBRegistry ||= OpenStruct.new(test: ConnectionAdapter.new("db/#{DBNAME}-test.db"), 
   development: ConnectionAdapter.new("db/#{DBNAME}-development.db"), 
   production: ConnectionAdapter.new("db/#{DBNAME}-production.db")
-  )
+)
